e383b08a4d2bc4c2a102c824fb9da486bf9e9e3f
ZimboPro <dejagerleslie@gmail.com>
2026-01-29 08:04:47 +0200

    refactor: remove comments and unused dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     hash::{Hash, RandomState},
     path::{Path, PathBuf},
 };
@@ -41,7 +41,7 @@ pub fn get_unreferenced_files(args: cli::Options) -> anyhow::Result<()> {
     };
 
     let registered_assets: HashSet<PathBuf> = assets.iter().map(|x| x.path.clone()).collect();
-    // info!("{} assets registered", assets.len());
+
     let mut deps: HashSet<String> = if args.deps {
         pubspec.dependencies.keys().cloned().collect()
     } else {
@@ -50,28 +50,16 @@ pub fn get_unreferenced_files(args: cli::Options) -> anyhow::Result<()> {
     // TODO allow to set entry point
     localisation::set_class_name(&pubspec.flutter_intl.class_name)?;
     let main = PathBuf::from("lib/main.dart");
-    // extracted_data.referenced_files.insert(main.clone());
-
-    // extract_data(
-    //     &main,
-    //     &pubspec.name,
-    //     &mut extracted_data,
-    //     &mut deps,
-    //     &mut assets,
-    //     &args,
-    // )?;
 
     let dart = glob("lib/**/*.dart").expect("Failed to read glob pattern");
     let dart: Vec<PathBuf> = dart.flatten().collect();
-    // let mut assets_set: dashmap::DashSet<AssetItem> =
-    //     dashmap::DashSet::from_iter(assets.into_iter());
     let mut assets_set: papaya::HashSet<AssetItem, RandomState> =
         papaya::HashSet::from_iter(assets);
 
     let locator: papaya::HashMap<String, bool> = papaya::HashMap::with_capacity(dart.len() / 10);
     let labels: papaya::HashSet<String> = papaya::HashSet::with_capacity(dart.len() / 10);
 
-    let results: dashmap::DashMap<PathBuf, Data> = dart
+    let results: HashMap<PathBuf, Data> = dart
         .clone()
         .into_par_iter()
         .map(|path| {
@@ -252,6 +240,7 @@ fn extract_single_file(
         }
     }
 
+    #[allow(clippy::mutable_key_type)]
     let mut referenced_asset_files = HashSet::with_capacity(10);
 
     let mut assets = assets.pin();
@@ -298,7 +287,7 @@ fn extract_single_file(
 
 fn collapse_list(
     path: &PathBuf,
-    files: &dashmap::DashMap<PathBuf, Data>,
+    files: &HashMap<PathBuf, Data>,
     referenced: &mut HashSet<PathBuf>,
     assets: &mut papaya::HashSet<AssetItem, RandomState>,
     deps: &mut HashSet<String>,
@@ -324,150 +313,3 @@ fn collapse_list(
         assets.remove(asset);
     }
 }
-
-// fn extract_data(
-//     file_path: &std::path::PathBuf,
-//     package_name: &str,
-//     extracted_data: &mut ExtractData,
-//     deps: &mut Vec<String>,
-//     assets: &mut Vec<AssetItem>,
-//     args: &cli::Options,
-// ) -> anyhow::Result<()> {
-//     let contents = std::fs::read_to_string(file_path)
-//         .unwrap_or_else(|_| panic!("Failed to read file: {:?}", file_path));
-//     for line in contents.lines() {
-//         if let Ok((_, dart)) = parser::dart_file(line) {
-//             match dart {
-//                 parser::DartFile::Import(path) => {
-//                     // relative path imports
-//                     let file = path.replace("%20", " ");
-//                     let file = Path::new(&file);
-//                     let file = file_path.parent().unwrap().join(file);
-//                     if !extracted_data
-//                         .referenced_files
-//                         .contains(&file.to_path_buf())
-//                     {
-//                         extracted_data
-//                             .referenced_files
-//                             .insert(file.parse_dot().unwrap().to_path_buf());
-//                         extract_data(
-//                             &file.parse_dot().unwrap().to_path_buf(),
-//                             package_name,
-//                             extracted_data,
-//                             deps,
-//                             assets,
-//                             args,
-//                         )?;
-//                     }
-//                 }
-//                 parser::DartFile::Package(name, mut path) => {
-//                     // package imports
-//                     if name == package_name {
-//                         path.insert_str(0, "lib");
-//                         let path = path.replace("%20", " ");
-//                         let file = Path::new(&path);
-//                         if !extracted_data
-//                             .referenced_files
-//                             .contains(&file.to_path_buf())
-//                         {
-//                             extracted_data.referenced_files.insert(file.to_path_buf());
-//                             extract_data(
-//                                 &file.to_path_buf(),
-//                                 package_name,
-//                                 extracted_data,
-//                                 deps,
-//                                 assets,
-//                                 args,
-//                             )?;
-//                         }
-//                     } else {
-//                         // referenced_packages.push(DartFile::Package(name, path));
-//                         // Remove deps used in referenced files
-//                         deps.retain(|x| x != &name);
-//                     }
-//                 }
-//                 parser::DartFile::Part(value) => {
-//                     // part files
-//                     let mut file = file_path.clone();
-//                     file.set_file_name(value);
-//                     extracted_data.referenced_files.insert(file);
-//                 }
-//                 parser::DartFile::Export(path) => {
-//                     let file = path.replace("%20", " ");
-//                     let file = Path::new(&file);
-//                     let file = file_path.parent().unwrap().join(file);
-//                     if !extracted_data
-//                         .referenced_files
-//                         .contains(&file.to_path_buf())
-//                     {
-//                         extracted_data
-//                             .referenced_files
-//                             .insert(file.parse_dot().unwrap().to_path_buf());
-//                         extract_data(
-//                             &file.parse_dot().unwrap().to_path_buf(),
-//                             package_name,
-//                             extracted_data,
-//                             deps,
-//                             assets,
-//                             args,
-//                         )?;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-//     let mut remove = false;
-//     let mut referenced_asset_files = HashSet::with_capacity(10);
-//     for asset in assets.iter() {
-//         if contents.contains(asset.borrow_file_name()) {
-//             remove = true;
-//             referenced_asset_files.insert(asset.borrow_path().clone());
-//         }
-//     }
-//     // Remove referenced assets from the set to speed up future checks
-//     if remove {
-//         assets.retain(|asset| !referenced_asset_files.contains(asset.borrow_path()));
-//     }
-
-//     remove = false;
-//     let mut used_deps = HashSet::with_capacity(10);
-//     for dep in deps.iter() {
-//         if contents.contains(dep) {
-//             remove = true;
-//             used_deps.insert(dep.clone());
-//         }
-//     }
-
-//     // Remove used deps from the set to speed up future checks
-//     if remove {
-//         deps.retain(|dep| !used_deps.contains(dep));
-//     }
-
-//     if args.labels {
-//         let s = all_localisation(&contents);
-//         if let Ok((_, keys)) = s {
-//             for key in keys {
-//                 extracted_data.labels_referenced.insert(key.to_owned());
-//             }
-//         }
-//     }
-
-//     if args.loc
-//         && let Ok((_, r)) = locator::locator(&contents)
-//     {
-//         for reg in r {
-//             match reg {
-//                 locator::Locator::Register(s) => {
-//                     extracted_data.locators.entry(s).or_insert(false);
-//                 }
-//                 locator::Locator::Get(s) => {
-//                     extracted_data.locators.insert(s, true);
-//                 }
-//                 _ => {}
-//             }
-//         }
-//     }
-
-//     Ok(())
-// }
